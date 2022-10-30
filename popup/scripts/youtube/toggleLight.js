b2d6ac9fc2f8230d2e38f567b02cbd91811ac0d3
Hoang Tran <99.hoangtran@gmail.com>
2022-10-31 00:44:46 +0700

    fix toggle light

--- popup/scripts/youtube/toggleLight.js
+++ popup/scripts/youtube/toggleLight.js
@@ -8,26 +8,17 @@ export default {
     vi: "Tắt/Mở đèn để tập trung xem video",
   },
   func: function () {
-    let key = "useful-scripts/youtube/togglelight";
-    let currentState = window.localStorage.getItem(key);
-    let newState = currentState == 1 ? 0 : 1;
-
-    let changed = false;
     ["#below", "#secondary", "#masthead-container"].forEach((_) => {
       let dom = document.querySelector(_);
       if (dom) {
-        dom.style.opacity = Number(newState);
-        changed = true;
+        let current = dom.style.opacity || 1;
+        let newValue = current == 1 ? 0 : 1;
+        dom.style.opacity = newValue;
       } else alert("ERROR: Cannot find element" + _);
     });
 
     document.querySelector("#player-theater-container")?.scrollIntoView?.({
       behavior: "smooth",
     });
-
-    changed && window.localStorage.setItem(key, newState);
-    window.onbeforeunload = () => {
-      window.localStorage.setItem(key, 1);
-    };
   },
 };
