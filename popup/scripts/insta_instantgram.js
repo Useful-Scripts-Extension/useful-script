91e4ca843cfee47d2308bcac1896f4b56b1383af
hoang.tran12 <hoang.tran12@mservice.com.vn>
2022-10-31 18:23:04 +0700

    instantgram (WIP)

---
+++ popup/scripts/insta_instantgram.js
@@ -0,0 +1,11 @@
+export default {
+  name: {
+    en: "Instantgram",
+    vi: "Instantgram",
+  },
+  description: {
+    en: "Download photo/video on instagram using instantgram",
+    vi: "Tải ảnh/video trên instagram bằng instantgram",
+  },
+  func: function () {},
+};
