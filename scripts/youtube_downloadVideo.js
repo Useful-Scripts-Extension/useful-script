691f75dbb24b08660032c967b6352171e57f4c61
Hoang Tran <99.hoangtran@gmail.com>
2022-11-21 02:43:51 +0700

    update youtube downloader

--- scripts/youtube_downloadVideo.js
+++ scripts/youtube_downloadVideo.js
@@ -8,26 +8,49 @@ export default {
     en: "Bypass age restriction, without login",
     vi: "Tải cả video giới hạn độ tuổi, không cần đăng nhập",
   },
-  blackList: [],
-  whiteList: [],
 
   func: function () {
+    // https://stackoverflow.com/a/8260383/11898496
+    function getIdFromYoutubeURL(url) {
+      var regExp =
+        /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
+      var match = url.match(regExp);
+      return match && match[1].length == 11 ? match[1] : false;
+    }
+
     let options = [
       {
         name: "yt1s.com",
-        url: "https://yt1s.com/vi/youtube-to-mp4?q=",
+        func: (url) => {
+          window.open("https://yt1s.com/vi/youtube-to-mp4?q=" + url);
+        },
       },
       {
         name: "10downloader.com",
-        url: "https://10downloader.com/download?v=",
-      },
-      {
-        name: "9xbuddy.com",
-        url: "https://9xbuddy.com/process?url=",
+        func: (url) => {
+          window.open("https://10downloader.com/download?v=" + url);
+        },
       },
       {
         name: "ymp4.com",
-        url: "https://ymp4.download/en50/?url=/",
+        func: (url) => {
+          window.open("https://ymp4.download/en50/?url=" + url);
+        },
+      },
+      {
+        name: "9xbuddy.com",
+        func: (url) => {
+          window.open("https://9xbuddy.com/process?url=" + url);
+        },
+      },
+      {
+        name: "getlinks.vip",
+        url: "https://getlinks.vip/vi/youtube/",
+        func: (url) => {
+          window.open(
+            "https://getlinks.vip/vi/youtube/" + getIdFromYoutubeURL(url)
+          );
+        },
       },
     ];
 
@@ -40,7 +63,7 @@ export default {
 
     if (choose != null && choose >= 0 && choose < options.length) {
       let url = window.prompt("Nhập link youtube:", location.href);
-      url && window.open(options[choose].url + url);
+      url && options[choose].func(url);
     }
   },
 };
