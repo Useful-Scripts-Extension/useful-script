060d93edfdf4ad5af46c07faeb025d325b754a9f
Hoang Tran <99.hoangtran@gmail.com>
2022-11-27 21:00:13 +0700

    move folder + instagram inject css

--- scripts/content-scripts/document_start.js
+++ scripts/content-scripts/document_start.js
@@ -1,6 +1,6 @@
 (async () => {
   // https://stackoverflow.com/a/53033388
-  const { injectScript, baseURL } = await import("./utils.js");
+  const { injectScript, baseURL, injectCss } = await import("./utils.js");
 
   // injectScript(baseURL + "track_settimeout.js");
   // injectScript(baseURL + "globals_debugger.js");
@@ -8,6 +8,9 @@
 
   if (location.hostname === "mp3.zing.vn")
     injectScript(baseURL + "mp3.zing.vn.js");
+
+  if (location.hostname === "www.instagram.com")
+    injectCss(baseURL + "instagram.css");
 
   // if (location.hostname === "luanxt.com")
   //   injectScriptFile("//code.jquery.com/jquery-3.6.1.min.js", true);
