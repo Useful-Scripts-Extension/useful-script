23181223b6d497e32b408472202b23e9ec704279
Hoang Tran <99.hoangtran@gmail.com>
2022-12-03 06:11:02 +0700

    gg drive download

--- scripts/ggdrive_downloadVideo.js
+++ scripts/ggdrive_downloadVideo.js
@@ -1,3 +1,10 @@
+import {
+  getCurrentTab,
+  openPopupWithHtml,
+  runScriptInCurrentTab,
+  showLoading,
+} from "./helpers/utils.js";
+
 export default {
   icon: "https://drive.google.com/favicon.ico",
   name: {
@@ -10,68 +17,105 @@ export default {
   },
   blackList: [],
   whiteList: [],
-  runInExtensionContext: false,
+  runInExtensionContext: true,
 
   func: async function () {
     // Post: https://www.facebook.com/groups/j2team.community/posts/974953859503401/
 
-    function parse(e) {
-      var result = {};
-      return (
-        e.split("&").forEach(function (e) {
-          result[decodeURIComponent(e.substring(0, e.indexOf("=")))] =
-            decodeURIComponent(e.substring(e.indexOf("=") + 1));
-        }),
-        result
-      );
+    async function getLinkVideoGDrive(docid) {
+      function parse(e) {
+        var result = {};
+        return (
+          e.split("&").forEach(function (e) {
+            result[decodeURIComponent(e.substring(0, e.indexOf("=")))] =
+              decodeURIComponent(e.substring(e.indexOf("=") + 1));
+          }),
+          result
+        );
+      }
+
+      function parseStream(e) {
+        var d = [];
+        return (
+          e.split(",").forEach(function (e) {
+            d.push(parse(e));
+          }),
+          d
+        );
+      }
+
+      async function getVideoLinkFromDocId(docid) {
+        let res = await fetch(
+          "https://drive.google.com/get_video_info?docid=" + docid
+        );
+
+        let text = await res.text();
+        let json = parse(text);
+
+        json.url_encoded_fmt_stream_map = parseStream(
+          json.url_encoded_fmt_stream_map
+        );
+
+        let result = json.url_encoded_fmt_stream_map.map(function (stream) {
+          let name = json.title.replace(/\+/g, " ");
+          return {
+            idfile: docid,
+            name: name,
+            quality: stream.quality,
+            url: stream.url,
+          };
+        });
+
+        console.log(result);
+        return result;
+      }
+
+      // let { id, title } = window?.viewerData?.config;
+      return await getVideoLinkFromDocId(docid);
     }
 
-    function parseStream(e) {
-      var d = [];
-      return (
-        e.split(",").forEach(function (e) {
-          d.push(parse(e));
-        }),
-        d
-      );
+    // https://stackoverflow.com/a/16840612
+    function getIdFromUrl(url) {
+      return url.match(/[-\w]{25,}(?!.*[-\w]{25,})/);
     }
 
-    function downloadVideo(videoInfo) {
-      window.open(videoInfo.url + "&filename=" + videoInfo.name);
-    }
-
-    async function getVideoLinkFromDocId(docid) {
-      let res = await fetch(
-        "https://drive.google.com/get_video_info?docid=" + docid
-      );
-
-      let text = await res.text();
-      let json = parse(text);
-
-      json.url_encoded_fmt_stream_map = parseStream(
-        json.url_encoded_fmt_stream_map
-      );
-
-      let result = json.url_encoded_fmt_stream_map.map(function (stream) {
-        let name = json.title.replace(/\+/g, " ");
-        return {
-          idfile: docid,
-          name: name,
-          quality: stream.quality,
-          url: stream.url,
-        };
+    let { closeLoading } = showLoading("Đang tìm link video...");
+    try {
+      let docid = await runScriptInCurrentTab(() => {
+        return window?.viewerData?.config?.id;
       });
 
-      console.log(result);
-      return result;
-    }
+      if (!docid) {
+        let tab = await getCurrentTab();
+        let url = prompt("Nhập link google drive video: ", tab.url);
+        if (url == null) return;
+        docid = getIdFromUrl(url);
+        if (!docid)
+          throw Error("Link không hợp lệ. Không tìm thấy id trong link.");
+      }
 
-    try {
-      let { id, title } = window.viewerData.config;
-      console.log("get link...");
-      await getVideoLinkFromDocId(id);
+      let res = await getLinkVideoGDrive(docid);
+      if (!res?.length) throw Error("Không tìm được link video.");
+      console.log(res);
+
+      openPopupWithHtml(
+        `<h1>${res[0].name}</h1>
+        ${res
+          .map((_) => {
+            let name = _.name.replace(/ /g, "_");
+            return `<div>
+              <h1><a href="${_.url}" target="_blank">${_.quality}</a></h1>
+              <video src="${_.url}" controls style="max-width:95%" />
+            </div>`;
+          })
+          .join("<br/>")}`,
+        700,
+        700
+      );
     } catch (e) {
       alert("ERROR: " + e);
+    } finally {
+      closeLoading();
     }
   },
 };
