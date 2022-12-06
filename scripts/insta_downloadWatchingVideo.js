a5f94b96f30fdf3e0ba2ad9158c1e9c8f199685e
Hoang Tran <99.hoangtran@gmail.com>
2022-12-07 05:21:26 +0700

    .

---
+++ scripts/insta_downloadWatchingVideo.js
@@ -0,0 +1,26 @@
+export default {
+  icon: "",
+  name: {
+    en: "",
+    vi: "",
+  },
+  description: {
+    en: "",
+    vi: "",
+  },
+
+  // Chọn 1 trong 2 cách, xoá cách không dùng:
+
+  // Cách 1: Mở link web trong tab mới, không cần dùng func
+  link: "",
+
+  // Cách 2: Chạy script
+  blackList: [],
+  whiteList: [],
+  runInExtensionContext: false,
+
+  func: function () {},
+};
+
+// Những thuộc tính/hàm có thể chia sẻ cho cách scripts khác sử dụng sẽ được viết vào đây
+export const shared = {};
