9108f164784053d24aa61118453803c91fd24189
Hoang Tran <99.hoangtran@gmail.com>
2022-12-12 00:23:46 +0700

    start-idle-end run smooth

--- scripts/content-scripts/document_end.js
+++ scripts/content-scripts/document_end.js
@@ -1,9 +1,14 @@
+// (async () => {
+// const { injectScript, baseURL, injectCss } = await import("./utils.js");
+// if (
+//   location.hostname.includes("facebook.com") ||
+//   location.hostname.includes("messenger.com")
+// ) {
+//   injectScript(baseURL + "fb_invisible_message.js");
+// }
+// })();
+
 (async () => {
-  // const { injectScript, baseURL, injectCss } = await import("./utils.js");
-  // if (
-  //   location.hostname.includes("facebook.com") ||
-  //   location.hostname.includes("messenger.com")
-  // ) {
-  //   injectScript(baseURL + "fb_invisible_message.js");
-  // }
+  const response = await chrome.runtime.sendMessage({ type: "document_end" });
+  console.log("sent document_end to background", response);
 })();
--- scripts/content-scripts/document_idle.js
+++ scripts/content-scripts/document_idle.js
@@ -1,5 +1,10 @@
+// (async () => {
+// const { injectScript, injectCss, baseURL } = await import("./utils.js");
+// if (location.hostname === "movies.hdviet.com")
+//   injectScript(baseURL + "movie_hd_viet.js");
+// })();
+
 (async () => {
-  // const { injectScript, injectCss, baseURL } = await import("./utils.js");
-  // if (location.hostname === "movies.hdviet.com")
-  //   injectScript(baseURL + "movie_hd_viet.js");
+  const response = await chrome.runtime.sendMessage({ type: "document_idle" });
+  console.log("sent document_idle to background", response);
 })();
