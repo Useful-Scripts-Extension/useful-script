00ca72e283e296254af35698e82819229c26ca02
Hoang Tran <99.hoangtran@gmail.com>
2022-12-21 23:16:20 +0700

    update - go to any commit github

--- scripts/github_goToFirstCommit.js
+++ scripts/github_goToFirstCommit.js
@@ -1,100 +1,90 @@
+import { getCurrentTab, showLoading } from "./helpers/utils.js";
+
 export default {
-  icon: `<i class="fa-solid fa-backward-fast fa-lg"></i>`,
+  icon: `<i class="fa-solid fa-code-branch fa-lg"></i>`,
   name: {
-    en: "Go to first commit",
-    vi: "Đi tới commit đầu tiên",
+    en: "Go to any commit",
+    vi: "Đi tới commit bất kỳ",
   },
   description: {
-    en: "Go to first commit of github repo",
-    vi: "Đi tới commit đầu tiên của repo github",
+    en: "Go to any commit of github repo. Included first commit.",
+    vi: "Đi tới commit bất kỳ của repo github. Bao gồm cả commit đầu tiên.",
   },
 
-  whiteList: ["https://github.com/*"],
-
-  onClick: async function () {
-    // Source: https://github.com/FarhadG/init
-    // Modified by HoangTran
-
-    let args = window.location.pathname.match(
-      /\/([^\/]+\/[^\/]+)(?:\/tree\/([^\/]+))?/
-    );
-    // args[1] is the `orgname/repo` url fragment
-    // args[2] is the optional branch or hash
-
-    if (!args || !args[1]) {
-      alert("Vui lòng mở 1 trang repo github rồi chạy lại chức năng.");
-      return;
-    }
-
+  onClickExtension: async () => {
+    let { closeLoading, setLoadingText } = showLoading("Đang chờ nhập url...");
     try {
-      let res = await fetch(
-        "https://api.github.com/repos/" +
-          args[1] +
-          "/commits?sha=" +
-          (args[2] || "")
-      );
-      // the link header has additional urls for paging
-      // parse the original JSON for the case where no other pages exist
-      let results = await Promise.all([res.headers.get("link"), res.json()]);
-      // results[0] is the link
-      // results[1] is the first page of commits
+      let tab = await getCurrentTab();
+      let url = prompt("Nhập link của github repo: ", tab.url);
+      if (url == null) return;
 
-      let commits;
-      if (results[0]) {
-        // the link contains two urls in the form
-        // <https://github.com/...>; rel=blah, <https://github.com/...>; rel=thelastpage
-        // split the url out of the string
-        let pageurl = results[0].split(",")[1].split(";")[0].slice(2, -1);
-        let pageurlWithoutPage = pageurl.split("&page=")[0];
-        let count = pageurl.match(/page=(.*?)$/)[1];
+      if (!url.includes("github.com"))
+        throw Error(
+          "Link không đúng định dạng.\nLink ví dụ: https://github.com/HoangTran0410/useful-script"
+        );
 
-        let pageNumber;
-        while (true) {
-          pageNumber = window.prompt(
-            `Tìm thấy ${count} trang commits.\n` +
-              `Mỗi trang 30 commits => ~${30 * count} commits.\n\n` +
-              `Nhập số thứ tự trang muốn xem (1-${count}):\n` +
-              `(Nhập ${count} để xem commit đầu tiên)`,
-            1
-          );
-          if (pageNumber == null) return;
+      let repoName = shared.getRepoNameFromUrl(url);
+      let [user, repo] = repoName.split("/");
 
-          pageNumber = Number(pageNumber);
-          if (pageNumber >= 1 && pageNumber <= count) break;
-          else alert(`Lựa chọn không hợp lệ (1-${count}). Vui lòng chọn lại.`);
-        }
+      setLoadingText("Đang đếm số lượng commit...");
+      let totalCommits = await shared.getTotalCommitCount(user, repo);
 
-        // fetch the selected page
-        res = await fetch(pageurlWithoutPage + "&page=" + (pageNumber || "1"));
-        commits = await res.json();
-      } else {
-        // if no link, we know we're on the only page
-        commits = results[1];
-      }
-
-      // get the last commit and extract the url
+      setLoadingText("Đang chờ lựa chọn...");
+      let index;
       while (true) {
-        let commitIndex = prompt(
-          `Tìm thấy ${commits.length} commits trong trang này.\n` +
-            `Chọn vị trí commit muốn xem (1-${commits.length}):\n`,
+        index = prompt(
+          `Có ${totalCommits} commits trong '${repo}' của ${user} .\n\n` +
+            `Nhập commit muốn xem (1-${totalCommits}):\n` +
+            `  (1 là commit đầu tiên)`,
           1
         );
-        if (commitIndex == null) return;
-
-        let index = Number(commitIndex);
-        if (index >= 1 && index <= commits.length) {
-          window.open(commits[commits.length - index - 1].html_url);
-          break;
-        } else {
+        if (index == null) return;
+        if (index < 1 || index > totalCommits)
           alert(
-            `Lựa chọn không hợp lệ (1-${commits.length}). Vui lòng chọn lại.`
+            `Lựa chọn không hợp lệ (1-${totalCommits}). Vui lòng chọn lại.`
           );
-        }
+        else break;
       }
+
+      setLoadingText("Đang tìm link commit...");
+      let firstCommit = await shared.getCommitAtIndex(
+        user,
+        repo,
+        totalCommits - index + 1
+      );
+
+      if (firstCommit.html_url) window.open(firstCommit.html_url);
+      else throw new Error("Không tìm thấy link.");
     } catch (e) {
       alert("ERROR: " + e);
+    } finally {
+      closeLoading();
     }
   },
 };
 
-export function goToFirstCommit() {}
+export const shared = {
+  // Idea from: https://github.com/FarhadG/init
+  // https://github.com/HoangTran0410/useful-script => repoName = HoangTran0410/useful-script
+  getRepoNameFromUrl(url) {
+    let pathName = new URL(url).pathname;
+    let [repoName, branchOrHash] =
+      pathName.match(/\/([^\/]+\/[^\/]+)(?:\/tree\/([^\/]+))?/) || [];
+    return repoName.slice(1);
+  },
+  getTotalCommitCount: async (user, repo) => {
+    let res = await fetch(
+      `https://api.github.com/repos/${user}/${repo}/commits?per_page=1`
+    );
+    let linkInHeader = res.headers.get("link");
+    let commitCount = linkInHeader.match(/&page=(\d+)>; rel="last"/)?.[1];
+    return Number(commitCount);
+  },
+  getCommitAtIndex: async (user, repo, index) => {
+    let res = await fetch(
+      `https://api.github.com/repos/${user}/${repo}/commits?per_page=1&page=${index}`
+    );
+    let json = await res.json();
+    return json[0];
+  },
+};
