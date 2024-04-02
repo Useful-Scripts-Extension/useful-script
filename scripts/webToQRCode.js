b473ec52e67c0acfbcec3daf716d87fc61901e52
hoang.tran12 <hoang.tran12@mservice.com.vn>
2024-04-02 17:56:32 +0700

    remove unused code

--- scripts/webToQRCode.js
+++ scripts/webToQRCode.js
@@ -23,19 +23,5 @@ export default {
       url: chrome.runtime.getURL("/scripts/textToQRCode.html?text=" + url),
       title: "Text To QRCode",
     });
-
-    // var url =
-    //   "http://chart.apis.google.com/chart?cht=qr&chs=300x300&chl=" +
-    //   encodeURIComponent(location.href);
-    // w = open(
-    //   url,
-    //   "w",
-    //   "location=no,status=yes,menubar=no,scrollbars=no,resizable=yes,width=500,height=500,modal=yes,dependent=yes"
-    // );
-    // if (w) {
-    //   setTimeout("w.focus()", 1000);
-    // } else {
-    //   location = url;
-    // }
   },
 };
