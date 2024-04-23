30bf803f95883184c8f9905b7e07de9d5077a7e6
hoang.tran12 <hoang.tran12@mservice.com.vn>
2024-04-23 16:39:27 +0700

    set lang prompt

--- popup/helpers/lang.js
+++ popup/helpers/lang.js
@@ -24,7 +24,18 @@ export function toggleLang() {
 }
 
 export function getLang() {
-  if (!currentLangKey) currentLangKey = langSaver.get(LANG.vi);
+  if (!currentLangKey) currentLangKey = langSaver.get();
+  if (!currentLangKey) {
+    let index = prompt(
+      "Choose language: You can change it later at flag icon (top-right corner)\n\n" +
+        "Chọn ngôn ngữ: Bạn có thể đổi sau bằng cách click lá cờ góc trên bên phải tiện ích\n\n" +
+        "1. Tiếng Việt\n2. English",
+      1
+    );
+
+    currentLangKey = index == 2 ? LANG.en : LANG.vi;
+    langSaver.set(currentLangKey);
+  }
   return currentLangKey;
 }
 
