acb20cc9c4a65bfbfac532e6e34e62dea5e14f35
hoang.tran12 <hoang.tran12@mservice.com.vn>
2024-04-23 15:45:30 +0700

    fix: inject ufsGlobal to popup

--- popup/main.js
+++ popup/main.js
@@ -1,3 +1,8 @@
 import("./index.js").then(() => {
   document.querySelector("#loading-fullscreen")?.remove();
 });
+
+import("../scripts/content-scripts/ufs_global.js", (success, fail) => {
+  if (success) console.log("UfsGlobal injected");
+  else console.log("UfsGlobal injection failed: ", fail);
+});
