bd686b238ae719a95cf6e79c8566baef5b5c83f7
hoang.tran12 <hoang.tran12@mservice.com.vn>
2024-06-10 18:08:25 +0700

    fix

--- scripts/fb_getAllAlbumInformation.js
+++ scripts/fb_getAllAlbumInformation.js
@@ -10,21 +10,21 @@ export default {
   },
 
   popupScript: {
-    onClick: async () => {
+    onClick: () => {
       let ACCESS_TOKEN = prompt("Nhập access token của bạn vào đây");
       if (!ACCESS_TOKEN) return;
 
       let id = prompt("Nhập ID của user, group, page cần lấy albums", "");
       if (!id) return;
 
-      fetch(
-        `https://graph.facebook.com/v13.0/${id}/albums?fields=type,name,count,link,created_time&limit=100&access_token=${ACCESS_TOKEN}`
-      )
-        .then((res) => res.json())
-        .then((json) => {
-          console.log(json.data);
-          alert(
-            `Tìm được ${json.data?.length} algum.\nXem kết quả trong console của extension`
+      fb_getAllAlbumInfos(id, ACCESS_TOKEN)
+        .then((result) => {
+          console.log(result);
+          prompt(
+            `Tìm được ${result?.length} albums:\n` +
+              result.map((_) => _.name + ": " + _.count).join("\n") +
+              "\n\nCopy ngay:",
+            JSON.stringify(result)
           );
         })
         .catch((err) => {
@@ -34,3 +34,24 @@ export default {
     },
   },
 };
+
+export async function fb_getAllAlbumInfos(id, access_token) {
+  let result = [];
+  let after = "";
+  while (true) {
+    try {
+      const res = await fetch(
+        `https://graph.facebook.com/v13.0/${id}/albums?fields=type,name,count,link,created_time&limit=100&access_token=${access_token}&after=${after}`
+      );
+      const json = await res.json();
+      if (json.data) result = result.concat(json.data);
+
+      let nextAfter = json.paging?.cursors?.after;
+      if (!nextAfter || nextAfter === after) break;
+      after = nextAfter;
+    } catch (e) {
+      break;
+    }
+  }
+  return result;
+}
